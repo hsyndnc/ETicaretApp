86eb8d7cbf5df4b44a8a6635932a9fde870e0ba2
hsyndnc <huseyinndnc@gmail.com>
2026-02-13 02:38:39 +0300

    Proje dosyaları oluşturulması ve dummy data

---
+++ ETicaretAPI/Core/ETİcaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETİcaretAPI.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistance/Concrete/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using ETİcaretAPI.Domain.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ETicaretAPI.Persistance.Concrete
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            //Target type kullanımı
+            => new()
+            {
+
+                new(){Id =Guid.NewGuid(), Name="Apple", Price=100, Stock=10}, 
+                new(){Id =Guid.NewGuid(), Name="Grapes", Price=120, Stock=8}, 
+                new() {Id =Guid.NewGuid(), Name = "Orange", Price = 80, Stock = 15},
+                new(){ Id =Guid.NewGuid() ,Name="Banana", Price=50, Stock=20},
+                new (){Id =Guid.NewGuid(), Name="Mango", Price=150, Stock=5}
+            };
+        
+    }
+}
